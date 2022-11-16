0e3fdcf2153a28388f4d00222f3944cc6771c963
Servan Akçay <e227637@sbl.ch>
2022-11-16 14:46:07 +0100

    color change keys

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function handleKeyPress(ev) {
     const player = document.querySelector("#player")
+    if (ev.code === "Space") {
     player.setAttribute("cx", 100 * Math.random())
     player.setAttribute("cy", 100 * Math.random())
+    } else if (ev.key === "r") {
+        player.setAttribute("fill", "red")
+    } else if (ev.key === "g") {
+        player.setAttribute("fill", "green")
+    }
 }
 
 window,onkeydown = handleKeyPress
